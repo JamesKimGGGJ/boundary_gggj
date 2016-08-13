2120e31f518b1b315a2508fc25f4b4c75e19e44e
yulcat <alszmtm@naver.com>
2016-08-14 00:45:59 +0900

    폭풍 스크립트 작업 시작

---
+++ Assets/Prefab/MapObject/StormEffect.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StormEffect : MonoBehaviour {
+	public GameObject StormElement;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
