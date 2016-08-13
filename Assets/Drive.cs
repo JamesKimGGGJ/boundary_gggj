893c48e17cc2cccdd0504a4ff5ac750cf3342d8e
김대원 <eodnjs2998@gmail.com>
2016-08-13 21:57:24 +0900

    interpolation

--- Assets/Drive.cs
+++ Assets/Drive.cs
@@ -9,8 +9,8 @@ public class Drive : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		float translation = Input.GetAxis("Vertical1") * speed * Time.deltaTime;
-		float rotation = Input.GetAxis("Horizontal1") * rotationSpeed * Time.deltaTime;
+		float translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+		float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
 
 		transform.Translate(0, 0, translation);
 		transform.Rotate(0, rotation, 0);
