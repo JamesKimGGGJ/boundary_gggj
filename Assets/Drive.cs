bfea95b8fd23a0dc3db80ad6a892469692c1cd25
김대원 <eodnjs2998@gmail.com>
2016-08-13 21:48:11 +0900

    add skeleton code

---
+++ Assets/Drive.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Drive : MonoBehaviour {
+
+	public float speed = 10f;
+	public float rotationSpeed = 100f;
+
+	// Update is called once per frame
+	void Update () {
+		float translation = Input.GetAxis("Vertical1") * speed * Time.deltaTime;
+		float rotation = Input.GetAxis("Horizontal1") * rotationSpeed * Time.deltaTime;
+
+		transform.Translate(0, 0, translation);
+		transform.Rotate(0, rotation, 0);
+
+	}
+}
---
+++ Assets/LocalPlayerSetup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class LocalPlayerSetup : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if(isLocalPlayer){
+			GetComponent<Drive>().enabled = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
