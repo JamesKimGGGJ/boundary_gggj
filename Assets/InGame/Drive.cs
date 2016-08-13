29a60401c4e67f9df4e4f5cd02316a9eb8e3da57
HBJ <bomul0327@naver.com>
2016-08-14 00:39:07 +0900

    매우 뜨거운 고침

--- Assets/InGame/Drive.cs
+++ Assets/InGame/Drive.cs
@@ -6,14 +6,19 @@ public class Drive : MonoBehaviour {
 
 	public float speed = 10f;
 	public float rotationSpeed = 100f;
+	private Rigidbody2D rb;
 
+	void Start(){
+		rb = GetComponent<Rigidbody2D>();
+	}
 	// Update is called once per frame
 	void Update () {
 		float translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 		float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
 
-		transform.Translate(0, 0, translation);
-		transform.Rotate(0, rotation, 0);
+		rb.AddForce(new Vector3(translation, 0, 0));
+		//transform.Translate(translation, 0, 0);
+		transform.Rotate(0, 0, rotation);
 
 	}
 }
---
+++ Assets/InGame/HostGameManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class HostGameManager : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	bool CheckGameOver(out int winner)
+	{
+		winner = -1;
+
+		GameObject[] playerGameObject = GameObject.FindGameObjectsWithTag("Player");
+		int count = playerGameObject.Length;
+		if (count == 0) {
+			Debug.LogError("no player");
+			return false;
+		}
+
+		if (count != 1)
+			return false;
+
+		var netId = playerGameObject[0].GetComponent<NetworkIdentity>();
+		if (netId == null)
+		{
+			// TODO: 나거나 혹은 네트워크를 통해서 생성된게 아니다.
+			winner = -1;
+			return true;
+		}
+
+		winner = netId.clientAuthorityOwner.connectionId;
+		return true;
+	}
+
+	//게임오버
+	private  void TryGameOver(){
+		int winner;
+		if (CheckGameOver(out winner))
+		{
+			Debug.Log("game over: winner " + winner);
+		}
+	}
+
+
+	public void TryGameOverAfterUpdate()
+	{
+		Invoke("TryGameOver", 0.1f);
+	}
+}
