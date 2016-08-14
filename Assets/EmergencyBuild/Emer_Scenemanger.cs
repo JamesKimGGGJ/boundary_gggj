bf74d2e0f16cb656f933fa39665fe22361d1bdef
HBJ <bomul0327@naver.com>
2016-08-15 00:04:15 +0900

    제 1 긴급용 빌드

---
+++ Assets/EmergencyBuild/Emer_Scenemanger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.Networking;
+public class Emer_Scenemanger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//restart
+		if (Input.GetKeyDown ("r")) {
+			Destroy (gameObject);
+			SceneManager.LoadScene (0);
+		} else if(Input.GetButtonDown("Cancel")){
+			Application.Quit();
+		}
+	}
+}
---
+++ Assets/EmergencyBuild/Emer_UIButtonCtrl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class Emer_UIButtonCtrl : MonoBehaviour {
+
+	public Transform cameraSecondPosition;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	public void OnStartButton(){
+		DOTween.Init();
+		Camera.main.transform.DOMove(cameraSecondPosition.position,1).SetEase(Ease.OutCubic);
+		Camera.main.transform.DORotate(cameraSecondPosition.rotation.eulerAngles,1).OnComplete(MoveToNextScene);
+	}
+
+	void MoveToNextScene(){
+		SceneManager.LoadScene (1);
+	}
+}
