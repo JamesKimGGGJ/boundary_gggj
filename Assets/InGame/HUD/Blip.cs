0b7ed5ef90b2f897d77420bde53d54ef65c17b9e
Choi <Choi>
2016-08-14 18:57:37 +0900

    HUD 적 위치 표시

---
+++ Assets/InGame/HUD/Blip.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Blip : MonoBehaviour {
+
+    public GameObject target;
+    public int pid;
+    public float size;
+
+    RectTransform myRectTransform;
+    Map m;
+
+    void Awake()
+    {
+        Player.OnDie += OnPlayerDie;
+    }
+
+    // Use this for initialization
+    void Start () {
+        m = GameObject.Find("Main Camera").GetComponent<Map>();
+        myRectTransform = GetComponent<RectTransform>();
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if(target != null)
+        {
+            ShowBlip();
+        }
+	}
+
+    void ShowBlip()
+    {
+        Vector3 newP = m.cam.WorldToScreenPoint(target.transform.position);
+
+        Vector3 stageBorder = new Vector3(Screen.width, Screen.height, 0);
+        myRectTransform.localScale = new Vector2(size, size);
+
+        newP -= stageBorder / 2;
+
+        if (newP.x > m.map.max.x || newP.x < m.map.min.x
+         || newP.y > m.map.max.y || newP.y < m.map.min.y)
+        {
+            GetComponent<Image>().enabled = true;
+        }
+        else
+        {
+            GetComponent<Image>().enabled = false;
+        }
+
+
+        if (newP != Vector3.zero)
+        {
+            float tilt = Mathf.Atan(newP.y / newP.x + 0.1f);
+
+            if(newP.x > 0)
+                myRectTransform.localRotation = Quaternion.Euler(0, 0, (tilt * 180) / Mathf.PI + 90f);
+            else
+                myRectTransform.localRotation = Quaternion.Euler(0, 0, (tilt * 180) / Mathf.PI - 90f);
+        }
+
+        Debug.Log(newP);
+        newP = m.MoveInside(newP);
+        myRectTransform.localPosition = newP;
+    }
+
+    void OnPlayerDie(int netId)
+    {
+        GameObject[] playerGameObject = GameObject.FindGameObjectsWithTag("Player");
+        bool deadCheck = true;
+        foreach(var obj in playerGameObject)
+        {
+            if (obj.GetComponent<NetworkIdentity>().clientAuthorityOwner.connectionId == pid)
+                deadCheck = false;
+        }
+
+        if (deadCheck)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/InGame/HUD/Map.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Map : MonoBehaviour {
+
+    public GameObject blip;
+    public GameObject panel;
+    public GameObject targetPosition;
+    public Camera cam;
+    public float zoomLv;
+
+    Transform mainTarget;
+    public Rect map;
+
+    void Awake()
+    {
+        Player.OnSpawn += OnPlayerSpawn;
+        cam = GetComponent<Camera>();
+    }
+
+    // Use this for initialization
+    void Start () {
+        map = panel.GetComponent<RectTransform>().rect;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void SetMainTarget(Transform T)
+    {
+        mainTarget = T;
+    }
+
+    public Vector2 MoveInside(Vector3 p)
+    {
+        Vector2 screenBorder = new Vector2(Screen.width, Screen.height);
+
+        if (p.y > map.max.y)
+        {
+            p = new Vector3((p.x * map.max.y) / p.y, map.max.y, 0);
+        }
+        if(p.y < map.min.y)
+        {
+            p = new Vector3((p.x * map.min.y) / p.y, map.min.y, 0);
+        }
+        if(p.x > map.max.x)
+        {
+            p = new Vector3(map.max.x, (p.y * map.max.x) / p.x, 0);
+        }
+        if(p.x < map.min.x)
+        {
+            p = new Vector3(map.min.x, (p.y * map.min.x) / p.x, 0);
+        }
+
+        return p;
+    }
+
+    void OnPlayerSpawn(int pId, GameObject player)
+    {
+        GameObject b;
+        b = Instantiate(blip, transform.position, transform.rotation) as GameObject;
+        b.GetComponent<Blip>().pid = pId;
+        b.GetComponent<Blip>().target = player;
+
+        b.transform.SetParent(panel.transform);
+    }
+}
