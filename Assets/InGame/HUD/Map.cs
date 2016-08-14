80a6517780493d24ed64ad441e84395e28ac496f
Choi <Choi>
2016-08-15 00:02:22 +0900

    HUD 적용

---
+++ Assets/InGame/HUD/Map.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Map : MonoBehaviour {
+
+    public GameObject blip;
+    public GameObject blipParent;
+    public GameObject camTarget;
+    public Camera cam;
+    public Rect map;
+
+    void Awake()
+    {
+        Player.OnSpawn += OnPlayerSpawn;
+    }
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        map = blipParent.GetComponent<RectTransform>().rect;
+    }
+
+    public bool MoveInside(Vector2 p)
+    {
+        if (p.y > map.max.y)
+        {
+            return false;
+        }
+        if (p.y < map.min.y)
+        {
+            return false;
+        }
+        if (p.x > map.max.x)
+        {
+            return false;
+        }
+        if (p.x < map.min.x)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    public Vector2 ReLocateBlip(Vector2 p)
+    {
+        if (p.y > map.max.y)
+        {
+            p = new Vector2((p.x * map.max.y) / p.y, map.max.y);
+        }
+        if (p.y < map.min.y)
+        {
+            p = new Vector2((p.x * map.min.y) / p.y, map.min.y);
+        }
+        if (p.x > map.max.x)
+        {
+            p = new Vector2(map.max.x, (p.y * map.max.x) / p.x);
+        }
+        if (p.x < map.min.x)
+        {
+            p = new Vector2(map.min.x, (p.y * map.min.x) / p.x);
+        }
+
+        return p;
+    }
+
+
+    void OnPlayerSpawn(int pId, GameObject player)
+    {
+        if (player.GetComponent<NetworkIdentity>().isLocalPlayer)
+        {
+            camTarget = player;
+        }
+
+        GameObject b;
+        b = Instantiate(blip, transform.position, Quaternion.Euler(0f, 0f, 0f)) as GameObject;
+        b.GetComponent<Blip>().pId = pId;
+        b.GetComponent<Blip>().target = player;
+        b.transform.SetParent(blipParent.transform);
+    }
+
+}
